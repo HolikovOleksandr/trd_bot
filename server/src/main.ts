13d952883dabeebab2880c405d8d328416d1572a
LesHolikov <sanyagolikov97@gmoil.com>
2024-06-15 01:53:16 +0300

    Connected to postgres

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const logger = new Logger(bootstrap.name);
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get<number>('port');
+
+  await app.listen(port);
+  logger.log(`🦾 Server started on http://127:0.0.1:${port}`);
 }
 bootstrap();
